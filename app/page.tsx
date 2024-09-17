cc2dcb732271ce91337cfd7460e4395fd0381110
Janak singh dhami <info@jsdhami.com.np>
2024-09-17 22:03:07 +0545

    added buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Footer from '../components/footer/footer'
 import Header from '../components/header/header'
+import { Button } from '@/components/ui/button'
 const page = () => {
   return (
    <>
@@ -8,6 +9,10 @@ const page = () => {
     <div className="flex flex-col items-center min-h-screen justify-center bg-inherit">
       <h1 className="text-6xl font-bold text-center pb-6">Welcome to TriScan!</h1>
       <p className="text-xl text-center">This is a Free & Open Source Plagiarism-Checker Software.</p>
+      <div className="flex items-center justify-center gap-4">
+        <Button className="mt-8">Get Started</Button>
+        <Button className="mt-8">Donate Now</Button>
+      </div>
     </div>
     <Footer/>
    </>
