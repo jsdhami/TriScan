e12b1dbbc759d6e525dae4bad2df9588e8836fd9
Janak Singh Dhami <139155991+jsdhami@users.noreply.github.com>
2024-09-17 19:14:41 +0000

    fixed some headers and footers

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
-import Footer from '../components/footer/footer'
-import Header from '../components/header/header'
 import { Button } from '@/components/ui/button'
 const page = () => {
   return (
    <>
-    <Header/>
     <div className="flex flex-col items-center min-h-screen justify-center bg-inherit">
       <h1 className="text-6xl font-bold text-center pb-6">Welcome to TriScan!</h1>
       <p className="text-xl text-center">This is a Free & Open Source Plagiarism-Checker Software.</p>
@@ -14,7 +11,6 @@ const page = () => {
         <Button className="mt-8">Donate Now</Button>
       </div>
     </div>
-    <Footer/>
    </>
   )
 }
